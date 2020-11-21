57ff42f6c4c4363896907bc09a9d25423556e1b5
Michal Cichra <michal@cichra.cz>
2020-11-21 10:09:25 +0100

    move with the submarine

--- Assets/Scripts/SubmarineController.cs
+++ Assets/Scripts/SubmarineController.cs
@@ -8,24 +8,29 @@ public class SubmarineController : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
+    private Vector3 movVec;
     [SerializeField] private float playerSpeed = 2.0f;
 
     private void Start()
     {
         controller = gameObject.AddComponent<CharacterController>();
     }
+    private void FixedUpdate()
+    {
+        controller.Move(movVec * (Time.deltaTime * playerSpeed));
+
+        if (playerVelocity != Vector3.zero)
+        {
+            gameObject.transform.forward = movVec;
+        }
+
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
 
     public void OnMove(InputValue input)
     {
         var inputVec = input.Get<Vector2>();
 
-        var moveVec = new Vector3(inputVec.x, inputVec.y, 0);
-        
-        controller.Move(moveVec * (Time.deltaTime * playerSpeed));
-
-        if (moveVec != Vector3.zero)
-        {
-            gameObject.transform.forward = moveVec;
-        }
+        movVec = new Vector3(inputVec.x, inputVec.y, 0);
     }
 }
