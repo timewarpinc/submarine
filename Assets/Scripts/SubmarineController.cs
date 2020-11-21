f80aee8194722271471fbc6310461518184cb154
Michal Cichra <michal@cichra.cz>
2020-11-21 01:44:56 +0100

    super simple movement

---
+++ Assets/Scripts/SubmarineController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SubmarineController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    [SerializeField] private float playerSpeed = 2.0f;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    public void OnMove(InputValue input)
+    {
+        var inputVec = input.Get<Vector2>();
+
+        var moveVec = new Vector3(inputVec.x, inputVec.y, 0);
+        
+        controller.Move(moveVec * (Time.deltaTime * playerSpeed));
+
+        if (moveVec != Vector3.zero)
+        {
+            gameObject.transform.forward = moveVec;
+        }
+    }
+}
